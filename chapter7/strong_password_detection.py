a05898f22c339eb20bcb20a76277efcda1ac1462
swchoi0102 <swchoi0102@gmail.com>
2017-04-10 23:54:32 +0900

    chapter7 project1

---
+++ chapter7/strong_password_detection.py
@@ -0,0 +1,29 @@
+import re
+
+
+def strong_check(password):
+    length_regex = re.compile(r"^[a-zA-Z0-9<>,.?/~`!@#$%^&*()_+|]{8,}$")
+    uppercase_regex = re.compile(r"[A-Z]")
+    lowercase_regex = re.compile(r"[a-z]")
+    digit_regx = re.compile(r"[0-9]")
+
+    if length_regex.search(password) is not None \
+            and uppercase_regex.search(password) is not None \
+            and lowercase_regex.search(password) is not None \
+            and digit_regx.search(password) is not None:
+        return True
+    else:
+        return False
+
+
+def main():
+    password = "Abcdefjg5"
+    print(strong_check(password))  # True
+    password = "abcedfged6"
+    print(strong_check(password))  # False
+    password = "12345678"
+    print(strong_check(password))  # False
+
+
+if __name__ == "__main__":
+    main()
