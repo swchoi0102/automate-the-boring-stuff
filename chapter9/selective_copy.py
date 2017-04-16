94fd44683dc5ac0b49c988e9fc83bce25e356832
swchoi0102 <swchoi0102@gmail.com>
2017-04-17 00:56:12 +0900

    chapter9 selective copy

---
+++ chapter9/selective_copy.py
@@ -0,0 +1,23 @@
+import shutil
+import os
+
+
+def selective_copy(folder, new_folder):
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+        print("Made new directory {}".format(new_folder))
+
+    for foldername, subfolders, filenames in os.walk(folder):
+        for filename in filenames:
+            if filename.endswith('.pdf') or filename.endswith('.jpg'):
+                shutil.copy(os.path.join(foldername, filename), new_folder)
+                print("Copy {}".format(os.path.join(foldername, filename)))
+
+
+def main():
+    folder = '/home/sangwoo/Downloads/'
+    new_folder = '/home/sangwoo/CopyFolder/'
+    selective_copy(folder, new_folder)
+
+if __name__ == "__main__":
+    main()
