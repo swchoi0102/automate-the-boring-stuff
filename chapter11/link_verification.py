3d8e44dff4ffb7da0235dd00f9ca4af29a342cd4
swchoi0102 <swchoi0102@gmail.com>
2017-04-27 23:18:37 +0900

    chapter11 link verification

---
+++ chapter11/link_verification.py
@@ -0,0 +1,16 @@
+import requests
+import bs4
+
+url = "http://imgur.com"
+res = requests.get(url)
+
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+for link in soup.find_all('a', href=True):
+    url = link['href']
+    if not url.startswith("http:") and not url.startswith("https:"):
+        url = "http:" + url
+    try:
+        res = requests.get(url)
+    except Exception:
+        print("broken link: {}".format(url))
