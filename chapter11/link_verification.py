0da4f8881f3d294fa3a57de8178af71526f60f99
swchoi0102 <swchoi0102@gmail.com>
2017-04-27 23:20:45 +0900

    fix

--- chapter11/link_verification.py
+++ chapter11/link_verification.py
@@ -9,8 +9,8 @@ soup = bs4.BeautifulSoup(res.text, 'html.parser')
 for link in soup.find_all('a', href=True):
     url = link['href']
     if not url.startswith("http:") and not url.startswith("https:"):
-        url = "http:" + url
+        url2 = "http:" + url
     try:
-        res = requests.get(url)
+        res = requests.get(url2)
     except Exception:
         print("broken link: {}".format(url))
