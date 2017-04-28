6f6d196a6d10a31b28d07d54b58034e5ced60b3c
swchoi0102 <swchoi0102@gmail.com>
2017-04-28 17:16:06 +0900

    chapter12 multiplication table maker fix

--- chapter12/multiplication_table_maker.py
+++ chapter12/multiplication_table_maker.py
@@ -2,27 +2,31 @@ import sys
 import openpyxl
 from openpyxl.styles import Font
 
-N = int(sys.argv[1])
 
-wb = openpyxl.Workbook()
-sheet = wb.active
+def main():
+    
+    N = int(sys.argv[1])
 
-bold_font = Font(bold=True)
+    wb = openpyxl.Workbook()
+    sheet = wb.active
 
-for row in range(1, N+2):
-    for col in range(1, N+2):
-        if row == 1 and col == 1:
-            continue
-        elif row == 1:
-            sheet.cell(row=row, column=col).value = col-1
-            sheet.cell(row=row, column=col).font = bold_font
-        elif col == 1:
-            sheet.cell(row=row, column=col).value = row-1
-            sheet.cell(row=row, column=col).font = bold_font
-        else:
-            sheet.cell(row=row, column=col).value = (col-1)*(row-1)
+    bold_font = Font(bold=True)
 
-wb.save('table.xlsx')
+    for row in range(1, N+2):
+        for col in range(1, N+2):
+            if row == 1 and col == 1:
+                continue
+            elif row == 1:
+                sheet.cell(row=row, column=col).value = col-1
+                sheet.cell(row=row, column=col).font = bold_font
+            elif col == 1:
+                sheet.cell(row=row, column=col).value = row-1
+                sheet.cell(row=row, column=col).font = bold_font
+            else:
+                sheet.cell(row=row, column=col).value = (col-1)*(row-1)
 
+    wb.save('table.xlsx')
 
+if __name__ == "__main__":
+    main()
 
