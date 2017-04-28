385faa7ec00d2fd6ea3957dc5ad65ee91aa856fc
swchoi0102 <swchoi0102@gmail.com>
2017-04-28 17:14:48 +0900

    chapter11 link verification fix

--- chapter11/link_verification.py
+++ chapter11/link_verification.py
@@ -1,16 +1,21 @@
 import requests
 import bs4
 
-url = "http://imgur.com"
-res = requests.get(url)
 
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
+def main():
+    url = "http://imgur.com"
+    res = requests.get(url)
 
-for link in soup.find_all('a', href=True):
-    url = link['href']
-    if not url.startswith("http:") and not url.startswith("https:"):
-        url2 = "http:" + url
-    try:
-        res = requests.get(url2)
-    except Exception:
-        print("broken link: {}".format(url))
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+    for link in soup.find_all('a', href=True):
+        url = link['href']
+        if not url.startswith("http:") and not url.startswith("https:"):
+            url2 = "http:" + url
+        try:
+            requests.get(url2)
+        except Exception:
+            print("broken link: {}".format(url))
+
+if __name__ == "__main__":
+    main()
