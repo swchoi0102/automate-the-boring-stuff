cba63c65a069745cf530e4655c6e0162eff34bc5
swchoi0102 <swchoi0102@gmail.com>
2017-04-28 17:13:29 +0900

    chapter11 image site downloader fix

--- chapter11/image_site_downloader.py
+++ chapter11/image_site_downloader.py
@@ -3,17 +3,20 @@ import bs4
 import shutil
 import os
 
-res = requests.get('http://imgur.com/')
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-if not os.path.exists("images"):
-    os.mkdir("images")
+def main():
+    res = requests.get('http://imgur.com/')
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-for idx, image in enumerate(soup.select('img[alt]')):
-    image_link = "http:" + image.get('src')
-    image_file = requests.get(image_link, stream=True)
-    with open('images/img_' + str(idx) + '.jpg', 'wb') as f:
-        shutil.copyfileobj(image_file.raw, f)
-    del image_file
+    if not os.path.exists("images"):
+        os.mkdir("images")
 
+    for idx, image in enumerate(soup.select('img[alt]')):
+        image_link = "http:" + image.get('src')
+        image_file = requests.get(image_link, stream=True)
+        with open('images/img_' + str(idx) + '.jpg', 'wb') as f:
+            shutil.copyfileobj(image_file.raw, f)
+        del image_file
 
+if __name__ == "__main__":
+    main()
